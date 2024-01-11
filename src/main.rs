dcdef49dab6a32b0358c5cbc7886b1b564f12694
George Daian <georgedaianx@gmail.com>
2024-01-11 16:16:51 +0200

    Handle PING command

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+const BUFFER_SIZE: usize = 1024;
+
+fn respond_with_pong(stream: &mut TcpStream) {
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Could not bind the listener");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                println!("Accepted new connection");
+                let mut buffer = [0; BUFFER_SIZE];
+                let size = _stream.read(&mut buffer).unwrap();
+                let command = String::from_utf8_lossy(&buffer[..size]);
+
+                match command.as_ref() {
+                    "*1\r\n$4\r\nping\r\n" => {
+                        respond_with_pong(&mut _stream);
+                    }
+                    _ => {
+                        println!("Unknown command: {:?}", command);
+                    }
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error: {}", e);
             }
         }
     }
