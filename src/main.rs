4ce1b43fcc1aee21f7946e222b76c5b6fea2cd91
George Daian <georgedaianx@gmail.com>
2024-01-14 11:43:59 +0200

    Extract handle connection into a function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::prelude::*;
+use std::io::{prelude::*, Error};
 use std::net::{TcpListener, TcpStream};
 
 const BUFFER_SIZE: usize = 1024;
@@ -10,34 +10,38 @@ fn respond_with_pong(stream: &mut TcpStream) {
     stream.flush().unwrap();
 }
 
+fn handle_connection(stream: Result<TcpStream, Error>) {
+    match stream {
+        Ok(mut _stream) => {
+            println!("Accepted new connection");
+            loop {
+                let mut buffer = [0; BUFFER_SIZE];
+                let size = _stream.read(&mut buffer).unwrap();
+                if size == 0 {
+                    break;
+                }
+                let command = String::from_utf8_lossy(&buffer[..size]);
+
+                match command.as_ref() {
+                    PING => {
+                        respond_with_pong(&mut _stream);
+                    }
+                    _ => {
+                        println!("Unknown command: {:?}", command);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            println!("Error at stream incoming: {}", e);
+        }
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Could not bind the listener");
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("Accepted new connection");
-                loop {
-                    let mut buffer = [0; BUFFER_SIZE];
-                    let size = _stream.read(&mut buffer).unwrap();
-                    if size == 0 {
-                        break;
-                    }
-                    let command = String::from_utf8_lossy(&buffer[..size]);
-
-                    match command.as_ref() {
-                        PING => {
-                            respond_with_pong(&mut _stream);
-                        }
-                        _ => {
-                            println!("Unknown command: {:?}", command);
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                println!("Error at stream incoming: {}", e);
-            }
-        }
+        handle_connection(stream);
     }
 }
