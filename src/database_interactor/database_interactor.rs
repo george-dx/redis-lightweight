1be0949910721b72200df5627d7678e45b7695d4
George Daian <georgedaianx@gmail.com>
2024-02-03 20:19:44 +0200

    Extract database interactor module

---
+++ src/database_interactor/database_interactor.rs
@@ -0,0 +1,55 @@
+use std::{io::Write, net::TcpStream, time::Duration};
+
+use crate::{database::database::Database, OK};
+
+pub struct DatabaseInteractor {
+    database: Database,
+}
+
+impl DatabaseInteractor {
+    pub fn new(database: Database) -> DatabaseInteractor {
+        DatabaseInteractor { database: database }
+    }
+
+    pub fn database_set(&mut self, stream: &mut TcpStream, command: &str) {
+        let dollar = "$";
+        let splitted_command = command.split(dollar).collect::<Vec<&str>>();
+        if let Some(value) = splitted_command.get(5) {
+            let expiry_in_ms = &value[1..].replace("\r\n", "");
+            match expiry_in_ms.parse::<u64>() {
+                Ok(conversion) => {
+                    let expiry_duration = Duration::from_millis(conversion);
+                    self.database.set(
+                        splitted_command[2].to_string(),
+                        splitted_command[3].to_string(),
+                        Some(expiry_duration),
+                    );
+                    let _ = stream.write(OK.as_bytes());
+                }
+                Err(e) => {
+                    println!("Error at u64 parse: {}", e);
+                    let _ = stream.write("+\r\n".as_bytes());
+                }
+            }
+        } else {
+            self.database.set(
+                splitted_command[2].to_string(),
+                splitted_command[3].to_string(),
+                None,
+            );
+            let _ = stream.write(OK.as_bytes());
+        }
+    }
+
+    pub fn database_get(&self, stream: &mut TcpStream, command: &str) {
+        let dollar = "$";
+        let splitted_command = command.split(dollar).collect::<Vec<&str>>();
+        if let Some(value) = self.database.get(splitted_command[2].to_string()) {
+            let response_parts = value.split("\r\n").collect::<Vec<&str>>();
+            let response = response_parts[1];
+            let _ = stream.write(format!("+{response}\r\n").as_bytes());
+        } else {
+            let _ = stream.write("$-1\r\n".as_bytes());
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod config;
 mod database;
+mod database_interactor;
 
 use config::config::Config;
 use database::database::Database;
+use database_interactor::database_interactor::DatabaseInteractor;
 use std::io::{prelude::*, Error};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::time::Duration;
 
 const BUFFER_SIZE: usize = 1024;
 const PING: &str = "*1\r\n$4\r\nping\r\n";
@@ -38,50 +39,9 @@ fn respond_with_pong(stream: &mut TcpStream) {
     stream.flush().unwrap();
 }
 
-fn database_set(database: &mut Database, stream: &mut TcpStream, command: &str) {
-    let dollar = "$";
-    let splitted_command = command.split(dollar).collect::<Vec<&str>>();
-    if let Some(value) = splitted_command.get(5) {
-        let expiry_in_ms = &value[1..].replace("\r\n", "");
-        match expiry_in_ms.parse::<u64>() {
-            Ok(conversion) => {
-                let expiry_duration = Duration::from_millis(conversion);
-                database.set(
-                    splitted_command[2].to_string(),
-                    splitted_command[3].to_string(),
-                    Some(expiry_duration),
-                );
-                let _ = stream.write(OK.as_bytes());
-            }
-            Err(e) => {
-                println!("Error at u64 parse: {}", e);
-                let _ = stream.write("+\r\n".as_bytes());
-            }
-        }
-    } else {
-        database.set(
-            splitted_command[2].to_string(),
-            splitted_command[3].to_string(),
-            None,
-        );
-        let _ = stream.write(OK.as_bytes());
-    }
-}
-
-fn database_get(database: &mut Database, stream: &mut TcpStream, command: &str) {
-    let dollar = "$";
-    let splitted_command = command.split(dollar).collect::<Vec<&str>>();
-    if let Some(value) = database.get(splitted_command[2].to_string()) {
-        let response_parts = value.split("\r\n").collect::<Vec<&str>>();
-        let response = response_parts[1];
-        let _ = stream.write(format!("+{response}\r\n").as_bytes());
-    } else {
-        let _ = stream.write("$-1\r\n".as_bytes());
-    };
-}
-
 fn handle_connection(stream: Result<TcpStream, Error>) {
-    let mut db = Database::new();
+    let db = Database::new();
+    let mut db_interactor = DatabaseInteractor::new(db);
     let mut config = Config::new();
     config.set();
     match stream {
@@ -99,9 +59,9 @@ fn handle_connection(stream: Result<TcpStream, Error>) {
             } else if command_str.contains(ECHO) {
                 respond_with_message(&mut _stream, command_str)
             } else if command_str.contains(SET) {
-                database_set(&mut db, &mut _stream, command_str);
+                db_interactor.database_set(&mut _stream, command_str);
             } else if command_str.contains(GET) && !command_str.contains(CONFIG) {
-                database_get(&mut db, &mut _stream, command_str);
+                db_interactor.database_get(&mut _stream, command_str);
             } else if command_str.contains(CONFIG) {
                 let message_type;
                 if command_str.contains("dir") {
